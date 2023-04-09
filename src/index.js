2643274bb53d73a2e60d353ef9cafb2fb1b27222
Michelle Heatherly <michelleheatherly218@gmail.com>
2023-04-09 15:42:14 -0800

    updates for using react snap to pre render html

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './assets/fonts/fonts.css';
+import './fonts.css';
 
 // Stylesheets
 import './index.css';
@@ -16,12 +16,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+if (rootElement.hasChildNodes()) {
+  root.hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
